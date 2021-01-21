071864347316c864458a8b9ec10580cdda82d97a
clentebanks <clentebanks0@gmail.com>
2021-01-20 19:52:44 -0600

    days

---
+++ Login/Login/Controllers/ClentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class ClentController : Controller
+    {
+        // GET: Clent
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            DateTime fechaInicio = DateTime.Parse("2021, 1, 10");
+            DateTime fechaActual = DateTime.Now;
+
+            // Calculo de dias.
+            TimeSpan td = fechaActual - fechaInicio;
+
+            // Total de dias.
+            //int differenceInDays = ts.Days;
+            ViewBag.totalDias = td.Days;
+
+
+            return View();
+        }
+    }
+}
