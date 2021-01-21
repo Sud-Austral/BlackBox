d4ba0c460db07b6fd39549a8e667cc13220e76ba
clentebanks <clentebanks0@gmail.com>
2021-01-21 10:01:05 -0600

    second

--- Login/Login/Controllers/ClentController.cs
+++ Login/Login/Controllers/ClentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Login.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,20 +10,35 @@ namespace Login.Controllers
     public class ClentController : Controller
     {
         // GET: Clent
-        public ActionResult Index()
+        public ActionResult Index(string fecha)
         {
-            ViewBag.Message = "Your application description page.";
+           
+            //ViewBag.Message = "Your application description page.";
 
-            DateTime fechaInicio = DateTime.Parse("2021, 1, 10");
+            DateTime fechaInicio = DateTime.Parse(fecha);
             DateTime fechaActual = DateTime.Now;
 
-            // Calculo de dias.
-            TimeSpan td = fechaActual - fechaInicio;
+             //Calculo de dias.
+             TimeSpan td = fechaActual - fechaInicio;
 
             // Total de dias.
-            //int differenceInDays = ts.Days;
-            ViewBag.totalDias = td.Days;
+           //int differenceInDays = ts.Days;   
+            ViewBag.totalDias = Util.fechaLimite(fecha);
+            if (ViewBag.totalDias)
+            {
+                return View();
 
+            }
+            else
+            {
+                return View("error");
+            }
+
+            
+        }
+
+        public ActionResult error()
+        {
 
             return View();
         }
---
+++ Login/Login/Models/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Login.Models
+{
+    public class Util
+    {
+        public static bool fechaLimite(string fecha)
+        {
+            bool salida = false;
+            int cantidadDias = 1;
+
+            DateTime fechaInicio = DateTime.Parse(fecha);
+            DateTime fechaActual = DateTime.Now;
+
+            //Calculo de dias.
+            TimeSpan td = fechaActual - fechaInicio;
+
+            salida = td.Days <= cantidadDias;
+
+            return salida;
+        }
+    }
+}
