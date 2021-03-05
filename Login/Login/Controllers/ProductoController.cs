62ab99dc0e314c0971cb384ee59f2640d73dc822
lmonsalve22 <lmonsalve22@gmail.com>
2021-03-05 15:55:00 -0300

    elecciones HN

---
+++ Login/Login/Controllers/ProductoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Elecciones_HN()
+        {
+            ViewBag.url = "https://odooutil.azurewebsites.net/design/eleccioneshn";
+            return View();
+        }
+    }
+}
