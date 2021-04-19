90526e861f5a22eadfed13c39cbd376bd263a323
gholley4 <gholley42@gmail.com>
2021-04-19 17:30:29 -0400

    commit

---
+++ AplicacionBlanco/AplicacionBlanco/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionBlanco.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Resultado()
+        {
+            return View();
+        }
+
+        public ActionResult Subscripciones()
+        {
+            return View();
+        }
+    }
+}
