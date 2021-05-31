290091388dcdfeb8d5b1ac105d6c135337b543f0
lmonsalve22 <lmonsalve22@gmail.com>
2021-05-31 19:13:11 -0400

    commit

--- Login/Login/Controllers/InfoController.cs
+++ Login/Login/Controllers/InfoController.cs
@@ -361,5 +361,28 @@ namespace AplicacionBlanco.Controllers
 
             return PartialView();
         }
+
+        public ActionResult index2()
+        {
+            int id = 1234;
+            var rand = new Random();
+
+            GRAFICO graf = new GRAFICO();
+            try
+            {
+                graf = dbGrafico.GRAFICO.Where(x => x.id == id).First();
+            }
+            catch (Exception)
+            {
+                graf = null;
+            }
+            if (graf.TIPO_GRAFICO_id > 1 || graf == null)
+            {
+                var listaGraficoAuxiliar = dbGrafico.GRAFICO.Where(x => x.TIPO_GRAFICO_id < 3).ToList();
+                graf = listaGraficoAuxiliar[rand.Next(listaGraficoAuxiliar.Count)];
+            }
+            ViewBag.Elemento = graf;//graficos
+            return View();
+        }
     }
 }
