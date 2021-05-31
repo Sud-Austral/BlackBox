d7a4f6418d11cddbc74f1063b8515448be336f5f
clentebanks <clentebanks0@gmail.com>
2021-05-30 23:55:13 -0600

    commit sunday1

--- Login/Login/Scripts/OcultarBoton.js
+++ Login/Login/Scripts/OcultarBoton.js
@@ -6,12 +6,12 @@ let button = document.querySelector('#boton').addEventListener('click', function
     if (!i) {
     /* document.getElementById('leerMas').style.display = 'inline';*/
         document.getElementById('leerMas').style.display = '';
-        document.getElementById('boton').innerHTML = 'Leer Menos';
+        document.getElementById('boton').innerHTML = 'Menos información';
         i = 1;
     }
     else {
         document.getElementById('leerMas').style.display = 'none';
-        document.getElementById('boton').innerHTML = 'Información del Gráfico';
+        document.getElementById('boton').innerHTML = 'Más información';
         i = 0;
     }
 
