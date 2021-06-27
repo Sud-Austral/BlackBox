348e57f239696830b2a315982b58217133a40b58
clentebanks <clentebanks0@gmail.com>
2021-06-27 01:22:18 -0600

    cccc

--- Login/Login/Controllers/InfoController.cs
+++ Login/Login/Controllers/InfoController.cs
@@ -408,5 +408,28 @@ namespace AplicacionBlanco.Controllers
 
             return PartialView();
         }
+
+        public ActionResult HomeOdoo2()
+        {
+            var rand = new Random();
+            List<int> aux = new List<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                aux.Add(rand.Next(dbGrafico.GRAFICO.Min(x => x.id), dbGrafico.GRAFICO.Max(x => x.id)));
+            }
+            var Graficos = dbGrafico.GRAFICO.Where(x => aux.Contains(x.id)).ToList();
+
+            //var Graficos = dbGrafico.GRAFICO.ToList();
+
+            List<GRAFICO> listaGraficos = new List<GRAFICO>();
+            for (int i = 0; i < 15; i++)
+            {
+                listaGraficos.Add(Graficos[rand.Next(Graficos.Count)]);
+            }
+            ViewBag.Graficos = listaGraficos;
+            return View();
+        }
     }
+
+
 }
