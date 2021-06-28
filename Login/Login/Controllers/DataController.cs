db593943ff0d3c1c3e1714f5ca8696dc40e18310
lmonsalve22 <lmonsalve22@gmail.com>
2021-06-28 13:19:26 -0400

    commit

--- Login/Login/Controllers/DataController.cs
+++ Login/Login/Controllers/DataController.cs
@@ -19,25 +19,6 @@ namespace Login.Controllers
         {
             return View();
         }
-
-        public ActionResult HomeOdoo()
-        {
-            var rand = new Random();
-            List<decimal> aux = new List<decimal>();
-            for (int i = 0; i < 20; i++)
-            {
-                aux.Add(rand.Next((int)dbGrafico.DATA_GRAFICO.Min(x => x.id), (int)dbGrafico.DATA_GRAFICO.Max(x => x.id)));
-            }
-            var Graficos = dbGrafico.DATA_GRAFICO.Where(x => aux.Contains(x.id)).ToList();
-            
-            List<DATA_GRAFICO> listaGraficos = new List<DATA_GRAFICO>();
-            for (int i = 0; i < 15; i++)
-            {
-                listaGraficos.Add(Graficos[rand.Next(Graficos.Count)]);
-            }
-            ViewBag.Graficos = listaGraficos;
-            
-            return View();
-        }
+        
     }
 }
