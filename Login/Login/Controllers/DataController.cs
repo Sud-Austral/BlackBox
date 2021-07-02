a6fb236b91d666811766c0dfb701094a25053373
clentebanks <clentebanks0@gmail.com>
2021-07-02 10:19:01 -0600

    commit friday1

--- Login/Login/Controllers/DataController.cs
+++ Login/Login/Controllers/DataController.cs
@@ -138,6 +138,10 @@ namespace Login.Controllers
         {
             return PartialView();
         }
+        public PartialViewResult IndexProductos()
+        {
+            return PartialView();
+        }
     }
 
 
