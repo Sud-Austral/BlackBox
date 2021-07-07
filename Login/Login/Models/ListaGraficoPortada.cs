51e0fe2edbd566b97bd4801c4806482b2a120fe7
lmonsalve22 <lmonsalve22@gmail.com>
2021-07-07 12:12:43 -0400

    avances

---
+++ Login/Login/Models/ListaGraficoPortada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Login.Models
+{
+    public class ListaGraficoPortada
+    {
+        public List<GraficoPortada> Graficos { get; set; }
+
+        public ListaGraficoPortada()
+        {
+            Graficos = new List<GraficoPortada>();
+            Graficos.Add(new GraficoPortada());
+            Graficos.Add(new GraficoPortada());
+            Graficos.Add(new GraficoPortada());
+            Graficos.Add(new GraficoPortada());
+            Graficos.Add(new GraficoPortada());
+            Graficos.Add(new GraficoPortada());
+            Graficos.Add(new GraficoPortada());
+            Graficos.Add(new GraficoPortada());
+            Graficos.Add(new GraficoPortada());
+        }
+    }
+}
