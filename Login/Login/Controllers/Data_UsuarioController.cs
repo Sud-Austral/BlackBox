8adb722045f97853a5bf0fbcec33f16835757d9f
clentebanks <clentebanks0@gmail.com>
2021-07-09 16:23:48 -0600

    commit friday 3

--- Login/Login/Controllers/Data_UsuarioController.cs
+++ Login/Login/Controllers/Data_UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Login.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +9,42 @@ namespace Login.Controllers
 {
     public class Data_UsuarioController : Controller
     {
+        private graficosEntities dbGrafico = new graficosEntities();
         // GET: Data_Usuario
         public ActionResult Index()
         {
             return View();
         }
-        public PartialViewResult Data_UsuarioGrafico()
+        public PartialViewResult Data_UsuarioGrafico(decimal id = 1234)
         {
+            ViewBag.time1 = DateTime.Now;
+            var rand = new Random();
+            DATA_GRAFICO graf = new DATA_GRAFICO();
+            try
+            {
+                graf = dbGrafico.DATA_GRAFICO.Where(x => x.id == id).First();
+            }
+            catch (Exception)
+            {
+                graf = null;
+            }
+            if (graf.TIPO_GRAFICO_id > 1 || graf == null)
+            {
+                var listaGraficoAuxiliar = dbGrafico.DATA_GRAFICO.Where(x => x.TIPO_GRAFICO_id < 3).ToList();
+                graf = listaGraficoAuxiliar[rand.Next(listaGraficoAuxiliar.Count)];
+            }
+            ViewBag.Elemento = graf;//graficos
+            // var listaAsociado = dbGrafico.PRODUCTO.Where(x => x.SECTOR_id == graf.CATEGORIA.PRODUCTO.SECTOR_id).ToList();
+            //var listaAsociado = dbGrafico.DATA_GRAFICO.Where(x => x.CATEGORIA.PRODUCTO.SECTOR_id == graf.CATEGORIA.PRODUCTO.SECTOR_id).ToList();
+
+            //List<int> aux = new List<int>();
+            //for (int i = 0; i < 50; i++)
+            //{
+            //    aux.Add(rand.Next(dbGrafico.DATA_GRAFICO.Min(x => x.id), dbGrafico.DATA_GRAFICO.Max(x => x.id)));
+            //}
+            //var Graficos = dbGrafico.DATA_GRAFICO.Where(x => aux.Contains(x.id)).ToList();
+            //ViewBag.Graficos = Graficos;//carrusel
+            ViewBag.time2 = DateTime.Now;
             return PartialView();
         }
         public PartialViewResult VisualizarGraficoColecion_Usuario()
@@ -34,6 +64,42 @@ namespace Login.Controllers
             return PartialView();
         }
         public PartialViewResult Recursos_Usuario()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Tematica()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Cosecha()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Empresas()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Empleados()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Produccion()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Precios()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Importaciones()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Exportaciones()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Rendimiento()
         {
             return PartialView();
         }
