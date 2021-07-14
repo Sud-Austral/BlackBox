b119e12ebcf67df5dc91236f3e31f8af87699739
lmonsalve22 <lmonsalve22@gmail.com>
2021-07-14 12:53:29 -0400

    commit

--- Login/Login/Models/ListaGraficoPortada.cs
+++ Login/Login/Models/ListaGraficoPortada.cs
@@ -12,6 +12,9 @@ namespace Login.Models
         public ListaGraficoPortada()
         {
             Graficos = new List<GraficoPortada>();
+            var aux = new GraficoPortada();
+            aux.url = "https://analytics.zoho.com/open-view/2395394000000943308?ZOHO_CRITERIA=%22Trasposicion_4.1%22.%22C%C3%B3digo_Regi%C3%B3n%22%20%3D%201";
+            Graficos.Add(aux);
             Graficos.Add(new GraficoPortada());
             Graficos.Add(new GraficoPortada());
             Graficos.Add(new GraficoPortada());
@@ -21,6 +24,7 @@ namespace Login.Models
             Graficos.Add(new GraficoPortada());
             Graficos.Add(new GraficoPortada());
             Graficos.Add(new GraficoPortada());
+            Graficos.Add(aux);
         }
     }
 }
